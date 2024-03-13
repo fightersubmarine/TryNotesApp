dc201abce583bfe399700c85b30852120d2aff86
Александр Соколов <133583532+fightersubmarine@users.noreply.github.com>
2024-03-13 10:03:30 +0300

    Initial Commit

---
+++ TryNotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryNotesApp
+//
+//  Created by Алина on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
